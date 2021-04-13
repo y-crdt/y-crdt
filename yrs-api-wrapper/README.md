cf59c7d9c9f9d2b3c664947e54872f8d81374889
Kevin Jahns <kevin.jahns@protonmail.com>
2021-04-13 15:20:44 +0200

    add yrs wrapper folder

---
+++ yrs-api-wrapper/README.md
@@ -0,0 +1,5 @@
+# Yrs API Wrapper
+
+Yrs exposes some internal APIs that can not be exported to language-bindings. The goal of this repository is to build a wrapper around Yrs that language
+wrappers can consume.
+
---
+++ yrs-api-wrapper/src/lib.rs
@@ -0,0 +1,34 @@
+
+// # Implements the alternative update API.
+// 
+// All of this is a polyfill that we can use to implement language bindings.
+// Eventually, we are going to implement these functions using exports from the
+// `yrs` package.
+//
+// Todos:
+// * Implement an Update struct with an API.
+// * Implement basic tests & examples.
+
+// Merge an array of document updates into a single document update
+pub fn merge_updates (updates: &[&[u8]]) -> Vec<u8> {
+    // @todo This is just a polyfill
+    let mut merged = Vec::new();
+    updates.into_iter().for_each(| update | {
+        update.into_iter().for_each(|b| {
+            merged.push(*b);
+        })
+    });
+    merged
+}
+
+// Computes the state vector from a document update
+pub fn encode_state_vector_from_update (update: &[u8]) -> Vec<u8> {
+    // @todo This is a polyfill that always returns an empty state vector
+    Vec::from([0])
+}
+
+// Encode the missing differences to another document update. 
+pub fn diff_updates (update: &[u8], state_vector: &[u8]) -> Vec<u8> {
+    // @todo This is a polyfill that always returns the complete state
+    update.to_owned()
+}
