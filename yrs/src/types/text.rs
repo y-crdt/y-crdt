986b938a9cc9618185b41692a7abc54556d6901a
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2021-05-24 15:33:01 +0200

    text test: insert concurrent elements at root

--- yrs/src/types/text.rs
+++ yrs/src/types/text.rs
@@ -72,6 +72,7 @@ impl Text {
 
 #[cfg(test)]
 mod test {
+    use crate::updates::encoder::Encode;
     use crate::Doc;
 
     #[test]
@@ -150,5 +151,35 @@ mod test {
         txt.insert(&mut txn, 6, " not");
 
         assert_eq!(txt.to_string(&txn).as_str(), "it was not expected");
+    }
+
+    #[test]
+    fn insert_root_concurrent() {
+        let mut d1 = Doc::with_client_id(1);
+        let mut t1 = d1.transact();
+        let mut txt1 = t1.get_text("test");
+
+        txt1.insert(&mut t1, 0, "hello ");
+
+        let mut d2 = Doc::with_client_id(2);
+        let mut t2 = d2.transact();
+        let mut txt2 = t2.get_text("test");
+
+        txt2.insert(&mut t2, 0, "world");
+
+        let d1_sv = d1.get_state_vector(&t1);
+        let d2_sv = d2.get_state_vector(&t2);
+
+        let u1 = d1.encode_delta_as_update(&d2_sv, &t1);
+        let u2 = d2.encode_delta_as_update(&d1_sv, &t2);
+
+        d1.apply_update(&mut t1, u2.as_slice());
+        d2.apply_update(&mut t2, u1.as_slice());
+
+        let a = txt1.to_string(&t1);
+        let b = txt2.to_string(&t2);
+
+        assert_eq!(a, b);
+        assert_eq!(a.as_str(), "hello world");
     }
 }
