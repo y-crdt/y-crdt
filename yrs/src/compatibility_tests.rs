3f07d8ecc17642103ae64408d9decca6e005a980
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2021-06-22 10:49:38 +0200

    added block-by-block checks to compatibility tests

--- yrs/src/compatibility_tests.rs
+++ yrs/src/compatibility_tests.rs
@@ -1,4 +1,9 @@
-use crate::Doc;
+use crate::block::{Block, BlockPtr, Item, ItemContent};
+use crate::id_set::{DeleteSet, IdSet};
+use crate::types::TypePtr;
+use crate::{Doc, ID};
+use std::cell::Cell;
+use std::rc::Rc;
 
 #[test]
 fn text_insert_delete() {
@@ -25,9 +30,85 @@ fn text_insert_delete() {
         97, 98, 193, 152, 234, 173, 126, 4, 152, 234, 173, 126, 0, 1, 129, 152, 234, 173, 126, 2,
         1, 132, 152, 234, 173, 126, 6, 2, 104, 105, 1, 152, 234, 173, 126, 2, 0, 3, 5, 2,
     ];
+    const CLIENT_ID: u64 = 264992024;
+    let expected_blocks = vec![
+        Block::Item(Item {
+            id: ID::new(CLIENT_ID, 0),
+            left: None,
+            right: None,
+            origin: None,
+            right_origin: None,
+            content: ItemContent::Deleted(3),
+            parent: TypePtr::Named("type".to_string()),
+            parent_sub: None,
+            deleted: false,
+        }),
+        Block::Item(Item {
+            id: ID::new(CLIENT_ID, 3),
+            left: None,
+            right: None,
+            origin: None,
+            right_origin: Some(ID::new(CLIENT_ID, 0)),
+            content: ItemContent::String("ab".to_string()),
+            parent: TypePtr::Id(BlockPtr::new(ID::new(CLIENT_ID, 0), 0)),
+            parent_sub: None,
+            deleted: false,
+        }),
+        Block::Item(Item {
+            id: ID::new(CLIENT_ID, 5),
+            left: None,
+            right: None,
+            origin: Some(ID::new(CLIENT_ID, 4)),
+            right_origin: Some(ID::new(CLIENT_ID, 0)),
+            content: ItemContent::Deleted(1),
+            parent: TypePtr::Id(BlockPtr::new(ID::new(CLIENT_ID, 4), 4)),
+            parent_sub: None,
+            deleted: false,
+        }),
+        Block::Item(Item {
+            id: ID::new(CLIENT_ID, 6),
+            left: None,
+            right: None,
+            origin: Some(ID::new(CLIENT_ID, 2)),
+            right_origin: None,
+            content: ItemContent::Deleted(1),
+            parent: TypePtr::Id(BlockPtr::new(ID::new(CLIENT_ID, 2), 2)),
+            parent_sub: None,
+            deleted: false,
+        }),
+        Block::Item(Item {
+            id: ID::new(CLIENT_ID, 7),
+            left: None,
+            right: None,
+            origin: Some(ID::new(CLIENT_ID, 6)),
+            right_origin: None,
+            content: ItemContent::String("hi".to_string()),
+            parent: TypePtr::Id(BlockPtr::new(ID::new(CLIENT_ID, 6), 6)),
+            parent_sub: None,
+            deleted: false,
+        }),
+    ];
+    let expected_ds = {
+        let mut ds = IdSet::new();
+        ds.insert(ID::new(CLIENT_ID, 0), 3);
+        ds.insert(ID::new(CLIENT_ID, 5), 2);
+        DeleteSet::from(ds)
+    };
+    let visited = Rc::new(Cell::new(false));
+    let setter = visited.clone();
+
     let mut doc = Doc::new();
+    let _sub = doc.on_update(move |e| {
+        for (actual, expected) in e.update.blocks().zip(expected_blocks.as_slice()) {
+            //println!("{}", actual);
+            assert_eq!(actual, expected);
+        }
+        assert_eq!(&e.delete_set, &expected_ds);
+        setter.set(true);
+    });
     let mut txn = doc.transact();
     let txt = txn.get_text("type");
     doc.apply_update(&mut txn, update);
     assert_eq!(txt.to_string(&txn), "abhi".to_string());
+    assert!(visited.get());
 }
