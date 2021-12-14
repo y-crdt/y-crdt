8721370ace0035a70959e8fc4d1688906cd34d4f
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2021-12-14 08:28:05 +0100

    stabilized benchmarks with statically generated input data

--- yrs/benches/benches.rs
+++ yrs/benches/benches.rs
@@ -1,410 +1,270 @@
 use criterion::*;
+use lib0::decoding::Cursor;
 use rand::distributions::Alphanumeric;
 use rand::prelude::ThreadRng;
 use rand::{thread_rng, Rng};
 use std::cell::Cell;
+use std::fs::File;
+use std::io::Read;
+use yrs::updates::decoder::{Decode, Decoder, DecoderV1};
+use yrs::updates::encoder::{Encode, Encoder};
 use yrs::{Array, Doc, Text};
 
+#[derive(PartialEq, Eq, Debug)]
 enum TextOp {
     Insert(u32, String),
     Delete(u32, u32),
 }
 
+impl Encode for TextOp {
+    fn encode<E: Encoder>(&self, encoder: &mut E) {
+        match self {
+            TextOp::Insert(idx, content) => {
+                encoder.write_u32(1);
+                encoder.write_u32(*idx);
+                encoder.write_string(content.as_str());
+            }
+            TextOp::Delete(idx, len) => {
+                encoder.write_u32(2);
+                encoder.write_u32(*idx);
+                encoder.write_u32(*len);
+            }
+        }
+    }
+}
+
+impl Decode for TextOp {
+    fn decode<D: Decoder>(decoder: &mut D) -> Self {
+        match decoder.read_u32() {
+            1 => {
+                let idx = decoder.read_u32();
+                let content = decoder.read_string().to_string();
+                TextOp::Insert(idx, content)
+            }
+            2 => {
+                let idx = decoder.read_u32();
+                let len = decoder.read_u32();
+                TextOp::Delete(idx, len)
+            }
+            a => panic!("unrecognized TextOp case: {}", a),
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Debug)]
 enum ArrayOp {
-    Insert(u32, Vec<i32>),
+    Insert(u32, Vec<u32>),
     Delete(u32, u32),
 }
 
-fn text_bench(doc: &Doc, text: &Text, ops: &Vec<TextOp>) {
-    for op in ops.iter() {
-        let mut txn = doc.transact();
-        match op {
-            TextOp::Insert(idx, txt) => text.insert(&mut txn, *idx, txt),
-            TextOp::Delete(idx, len) => text.remove_range(&mut txn, *idx, *len),
+impl Encode for ArrayOp {
+    fn encode<E: Encoder>(&self, encoder: &mut E) {
+        match self {
+            ArrayOp::Insert(idx, content) => {
+                encoder.write_u32(1);
+                encoder.write_u32(*idx);
+                encoder.write_u32(content.len() as u32);
+                for i in content.iter() {
+                    encoder.write_u32(*i);
+                }
+            }
+            ArrayOp::Delete(idx, len) => {
+                encoder.write_u32(2);
+                encoder.write_u32(*idx);
+                encoder.write_u32(*len);
+            }
         }
     }
 }
 
-fn array_bench(doc: &Doc, array: &Array, ops: &Vec<ArrayOp>) {
-    for op in ops.iter() {
-        let mut txn = doc.transact();
-        match op {
-            ArrayOp::Insert(idx, values) => array.insert_range(&mut txn, *idx, values.clone()),
-            ArrayOp::Delete(idx, len) => array.remove_range(&mut txn, *idx, *len),
+impl Decode for ArrayOp {
+    fn decode<D: Decoder>(decoder: &mut D) -> Self {
+        match decoder.read_u32() {
+            1 => {
+                let idx = decoder.read_u32();
+                let mut i = decoder.read_u32() as usize;
+                let mut content = Vec::with_capacity(i);
+                while i > 0 {
+                    content.push(decoder.read_u32());
+                    i -= 1;
+                }
+                ArrayOp::Insert(idx, content)
+            }
+            2 => {
+                let idx = decoder.read_u32();
+                let len = decoder.read_u32();
+                ArrayOp::Delete(idx, len)
+            }
+            _ => panic!("unrecognized TextOp case"),
         }
     }
+}
+
+fn encode_vec<T: Encode, E: Encoder>(vec: &Vec<T>, encoder: &mut E) {
+    encoder.write_u32(vec.len() as u32);
+    for value in vec.iter() {
+        value.encode(encoder);
+    }
+}
+
+fn decode_vec<T: Decode, D: Decoder>(decoder: &mut D) -> Vec<T> {
+    let mut i = decoder.read_u32() as usize;
+    let mut vec = Vec::with_capacity(i);
+    while i > 0 {
+        vec.push(T::decode(decoder));
+        i -= 1;
+    }
+    vec
+}
+
+fn load<T: Decode>(src: &str) -> Vec<T> {
+    let mut f = File::open(src).unwrap();
+    let mut buf = Vec::new();
+    f.read_to_end(&mut buf).unwrap();
+    let mut decoder = DecoderV1::new(Cursor::new(buf.as_slice()));
+    decode_vec(&mut decoder)
+}
+
+fn text_benchmark(c: &mut Criterion, name: &str, src: &str) {
+    let input = {
+        let doc = Doc::new();
+        let mut txn = doc.transact();
+        let txt = txn.get_text("text");
+        let ops = load(src);
+        (doc, txt, ops)
+    };
+
+    c.bench_with_input(
+        BenchmarkId::new(name, input.2.len()),
+        &input,
+        |b, (doc, text, ops)| {
+            b.iter(|| {
+                for op in ops.iter() {
+                    let mut txn = doc.transact();
+                    match op {
+                        TextOp::Insert(idx, txt) => text.insert(&mut txn, *idx, txt),
+                        TextOp::Delete(idx, len) => text.remove_range(&mut txn, *idx, *len),
+                    }
+                }
+            });
+        },
+    );
+}
+
+fn array_benchmark(c: &mut Criterion, name: &str, src: &str) {
+    let input = {
+        let doc = Doc::new();
+        let mut txn = doc.transact();
+        let array = txn.get_array("text");
+        let ops = load(src);
+        (doc, array, ops)
+    };
+
+    c.bench_with_input(
+        BenchmarkId::new(name, input.2.len()),
+        &input,
+        |b, (doc, array, ops)| {
+            b.iter(|| {
+                for op in ops.iter() {
+                    let mut txn = doc.transact();
+                    match op {
+                        ArrayOp::Insert(idx, values) => {
+                            array.insert_range(&mut txn, *idx, values.clone())
+                        }
+                        ArrayOp::Delete(idx, len) => array.remove_range(&mut txn, *idx, *len),
+                    }
+                }
+            });
+        },
+    );
 }
 
 fn b1_1(c: &mut Criterion) {
-    let mut group = c.benchmark_group("[B1.1] Append N characters");
-
-    fn gen_ops(size: usize) -> Vec<TextOp> {
-        let sample: Vec<_> = thread_rng()
-            .sample_iter(&Alphanumeric)
-            .take(size)
-            .map(|c| c.to_string())
-            .collect();
-
-        (0..size as u32)
-            .into_iter()
-            .zip(sample)
-            .map(|(i, str)| TextOp::Insert(i, str))
-            .collect()
-    }
-
-    for size in [1000, 3000, 6000] {
-        let input = {
-            let doc = Doc::new();
-            let mut txn = doc.transact();
-            let txt = txn.get_text("text");
-            (doc, txt, gen_ops(size as usize))
-        };
-
-        group.throughput(Throughput::Elements(size));
-        group.bench_with_input(
-            format!("[B1.1] Append {} characters", size),
-            &input,
-            |b, (doc, text, ops)| {
-                b.iter(|| text_bench(doc, text, ops));
-            },
-        );
-    }
-    group.finish();
+    text_benchmark(
+        c,
+        "[B1.1] Append 6000 characters",
+        "./yrs/benches/input/b-1-1-6000.bin",
+    );
 }
 
 fn b1_2(c: &mut Criterion) {
-    let mut group = c.benchmark_group("[B1.2] Insert string of length N");
-
-    for size in [1000, 3000, 6000] {
-        let input = {
-            let doc = Doc::new();
-            let mut txn = doc.transact();
-            let txt = txn.get_text("text");
-            let s: String = thread_rng()
-                .sample_iter(&Alphanumeric)
-                .take(size as usize)
-                .collect();
-            (doc, txt, vec![TextOp::Insert(0, s)])
-        };
-
-        group.throughput(Throughput::Elements(size));
-        group.bench_with_input(
-            format!("[B1.2] Insert string of length {}", size),
-            &input,
-            |b, (doc, txt, ops)| {
-                b.iter(|| text_bench(doc, txt, ops));
-            },
-        );
-    }
-    group.finish();
+    text_benchmark(
+        c,
+        "[B1.2] Insert string of length 6000",
+        "./yrs/benches/input/b-1-2-6000.bin",
+    );
 }
 
 fn b1_3(c: &mut Criterion) {
-    let mut group = c.benchmark_group("[B1.3] Prepend N characters");
-
-    fn gen_ops(size: usize) -> Vec<TextOp> {
-        let sample: Vec<_> = thread_rng()
-            .sample_iter(&Alphanumeric)
-            .take(size)
-            .map(|c| c.to_string())
-            .collect();
-
-        (0..size as u32)
-            .into_iter()
-            .zip(sample)
-            .map(|(i, str)| TextOp::Insert(0, str))
-            .collect()
-    }
-
-    for size in [1000, 3000, 6000] {
-        let input = {
-            let doc = Doc::new();
-            let mut txn = doc.transact();
-            let txt = txn.get_text("text");
-            (doc, txt, gen_ops(size as usize))
-        };
-
-        group.throughput(Throughput::Elements(size));
-        group.bench_with_input(
-            format!("[B1.3] Prepend {} characters", size),
-            &input,
-            |b, (doc, text, ops)| {
-                b.iter(|| text_bench(doc, text, ops));
-            },
-        );
-    }
-    group.finish();
+    text_benchmark(
+        c,
+        "[B1.3] Prepend 6000 characters",
+        "./yrs/benches/input/b-1-3-6000.bin",
+    );
 }
 
 fn b1_4(c: &mut Criterion) {
-    let mut group = c.benchmark_group("[B1.4] Insert N characters at random positions");
-
-    fn gen_ops(size: usize) -> Vec<TextOp> {
-        let mut rng = thread_rng();
-        let sample: Vec<_> = rng
-            .sample_iter(&Alphanumeric)
-            .take(size)
-            .map(|c| c.to_string())
-            .collect();
-
-        (0..size as u32)
-            .into_iter()
-            .zip(sample)
-            .map(|(i, str)| {
-                let idx = rng.gen_range(0, i.max(1));
-                TextOp::Insert(idx, str)
-            })
-            .collect()
-    }
-
-    for size in [1000, 3000, 6000] {
-        let input = {
-            let doc = Doc::new();
-            let mut txn = doc.transact();
-            let txt = txn.get_text("text");
-            (doc, txt, gen_ops(size as usize))
-        };
-
-        group.throughput(Throughput::Elements(size));
-        group.bench_with_input(
-            format!("[B1.4] Insert {} characters at random positions", size),
-            &input,
-            |b, (doc, text, ops)| {
-                b.iter(|| text_bench(doc, text, ops));
-            },
-        );
-    }
-    group.finish();
+    text_benchmark(
+        c,
+        "[B1.4] Insert 6000 characters at random positions",
+        "./yrs/benches/input/b-1-4-6000.bin",
+    );
 }
 
 fn b1_5(c: &mut Criterion) {
-    let mut group = c.benchmark_group("[B1.5] Insert N words at random positions");
-
-    fn gen_string(rng: &mut ThreadRng, min: usize, max: usize) -> String {
-        let len = rng.gen_range(min, max);
-        rng.sample_iter(&Alphanumeric).take(len).collect()
-    }
-
-    fn gen_ops(size: usize) -> Vec<TextOp> {
-        let mut rng = thread_rng();
-        (0..size as u32)
-            .into_iter()
-            .map(|i| {
-                let str = gen_string(&mut rng, 2, 10);
-                let idx = rng.gen_range(0, i.max(1));
-                TextOp::Insert(idx, str)
-            })
-            .collect()
-    }
-
-    for size in [1000, 3000, 6000] {
-        let input = {
-            let doc = Doc::new();
-            let mut txn = doc.transact();
-            let txt = txn.get_text("text");
-            (doc, txt, gen_ops(size as usize))
-        };
-
-        group.throughput(Throughput::Elements(size));
-        group.bench_with_input(
-            format!("[B1.5] Insert {} words at random positions", size),
-            &input,
-            |b, (doc, text, ops)| {
-                b.iter(|| text_bench(doc, text, ops));
-            },
-        );
-    }
-    group.finish();
+    text_benchmark(
+        c,
+        "[B1.5] Insert 6000 words at random positions",
+        "./yrs/benches/input/b-1-5-6000.bin",
+    );
 }
 
 fn b1_6(c: &mut Criterion) {
-    let mut group = c.benchmark_group("[B1.6] Insert string, then delete it");
-
-    for size in [1000, 3000, 6000] {
-        let input = {
-            let doc = Doc::new();
-            let mut txn = doc.transact();
-            let txt = txn.get_text("text");
-            let s: String = thread_rng()
-                .sample_iter(&Alphanumeric)
-                .take(size as usize)
-                .collect();
-            let len = s.len() as u32;
-            (doc, txt, vec![TextOp::Insert(0, s), TextOp::Delete(0, len)])
-        };
-
-        group.throughput(Throughput::Elements(size));
-        group.bench_with_input(
-            format!("[B1.6] Insert string of length {}, then delete it", size),
-            &input,
-            |b, (doc, text, ops)| {
-                b.iter(|| text_bench(doc, text, ops));
-            },
-        );
-    }
-    group.finish();
+    text_benchmark(
+        c,
+        "[B1.6] Insert string, then delete it",
+        "./yrs/benches/input/b-1-6-6000.bin",
+    );
 }
 
 fn b1_7(c: &mut Criterion) {
-    let mut group = c.benchmark_group("[B1.7] Insert/Delete strings at random positions");
-
-    fn gen_ops(size: usize) -> Vec<TextOp> {
-        let mut rng = thread_rng();
-        let mut total_len = Cell::new(0u32);
-        (0..size as u32)
-            .into_iter()
-            .map(|i| {
-                let total = total_len.get();
-                let idx = if total == 0 {
-                    0
-                } else {
-                    rng.gen_range(0, total)
-                };
-                if total == idx || rng.gen_bool(0.5) {
-                    let str = {
-                        let len = rng.gen_range(2, 10);
-                        total_len.set(total + len);
-                        rng.sample_iter(&Alphanumeric).take(len as usize).collect()
-                    };
-                    TextOp::Insert(idx, str)
-                } else {
-                    let hi = (total - idx).min(9);
-                    let len = if hi == 1 { 1 } else { rng.gen_range(1, hi) };
-                    total_len.set(total - len);
-                    TextOp::Delete(idx, len)
-                }
-            })
-            .collect()
-    }
-
-    for size in [1000, 3000, 6000] {
-        let input = {
-            let doc = Doc::new();
-            let mut txn = doc.transact();
-            let txt = txn.get_text("text");
-            (doc, txt, gen_ops(size as usize))
-        };
-
-        group.throughput(Throughput::Elements(size));
-        group.bench_with_input(
-            format!("[B1.7] Insert/Delete {} strings at random positions", size),
-            &input,
-            |b, (doc, text, ops)| {
-                b.iter(|| text_bench(doc, text, ops));
-            },
-        );
-    }
-    group.finish();
+    text_benchmark(
+        c,
+        "[B1.7] Insert/Delete strings at random positions",
+        "./yrs/benches/input/b-1-7-6000.bin",
+    );
 }
 
 fn b1_8(c: &mut Criterion) {
-    let mut group = c.benchmark_group("[B1.8] Append N numbers");
-
-    for size in [1000, 3000, 6000] {
-        let input = {
-            let mut rng = thread_rng();
-            let doc = Doc::new();
-            let mut txn = doc.transact();
-            let array = txn.get_array("text");
-            let ops: Vec<ArrayOp> = (0..size)
-                .map(|i| ArrayOp::Insert(i, vec![rng.gen()]))
-                .collect();
-            (doc, array, ops)
-        };
-
-        group.throughput(Throughput::Elements(size as u64));
-        group.bench_with_input(
-            format!("[B1.8] Append {} numbers", size),
-            &input,
-            |b, (doc, array, ops)| {
-                b.iter(|| array_bench(doc, array, ops));
-            },
-        );
-    }
-    group.finish();
+    array_benchmark(
+        c,
+        "[B1.8] Append 6000 numbers",
+        "./yrs/benches/input/b-1-8-6000.bin",
+    );
 }
 
 fn b1_9(c: &mut Criterion) {
-    let mut group = c.benchmark_group("[B1.9] Insert Array of N numbers");
-
-    for size in [1000, 3000, 6000] {
-        let input = {
-            let mut rng = thread_rng();
-            let doc = Doc::new();
-            let mut txn = doc.transact();
-            let array = txn.get_array("text");
-            let sample: Vec<i32> = (0..size).map(|_| rng.gen()).collect();
-            (doc, array, vec![ArrayOp::Insert(0, sample)])
-        };
-
-        group.throughput(Throughput::Elements(size as u64));
-        group.bench_with_input(
-            format!("[B1.9] Insert Array of {} numbers", size),
-            &input,
-            |b, (doc, array, ops)| {
-                b.iter(|| array_bench(doc, array, ops));
-            },
-        );
-    }
-    group.finish();
+    array_benchmark(
+        c,
+        "[B1.9] Insert Array of 6000 numbers",
+        "./yrs/benches/input/b-1-9-6000.bin",
+    );
 }
 
 fn b1_10(c: &mut Criterion) {
-    let mut group = c.benchmark_group("[B1.10] Prepend N numbers");
-
-    for size in [1000, 3000, 6000] {
-        let input = {
-            let mut rng = thread_rng();
-            let doc = Doc::new();
-            let mut txn = doc.transact();
-            let array = txn.get_array("text");
-            let ops: Vec<ArrayOp> = (0..size)
-                .map(|i| ArrayOp::Insert(0, vec![rng.gen()]))
-                .collect();
-            (doc, array, ops)
-        };
-
-        group.throughput(Throughput::Elements(size as u64));
-        group.bench_with_input(
-            format!("[B1.10] Prepend {} numbers", size),
-            &input,
-            |b, (doc, array, ops)| {
-                b.iter(|| array_bench(doc, array, ops));
-            },
-        );
-    }
-    group.finish();
+    array_benchmark(
+        c,
+        "[B1.10] Prepend 6000 numbers",
+        "./yrs/benches/input/b-1-10-6000.bin",
+    );
 }
 
 fn b1_11(c: &mut Criterion) {
-    let mut group = c.benchmark_group("[B1.11] Insert N numbers at random positions");
-
-    for size in [1000, 3000, 6000] {
-        let input = {
-            let mut rng = thread_rng();
-            let doc = Doc::new();
-            let mut txn = doc.transact();
-            let array = txn.get_array("text");
-            let ops: Vec<ArrayOp> = (0..size)
-                .map(|i| {
-                    let idx = rng.gen_range(0, i.max(1));
-                    let values = vec![rng.gen()];
-                    ArrayOp::Insert(idx, values)
-                })
-                .collect();
-            (doc, array, ops)
-        };
-
-        group.throughput(Throughput::Elements(size as u64));
-        group.bench_with_input(
-            format!("[B1.11] Insert {} numbers at random positions", size),
-            &input,
-            |b, (doc, array, ops)| {
-                b.iter(|| array_bench(doc, array, ops));
-            },
-        );
-    }
-    group.finish();
+    array_benchmark(
+        c,
+        "[B1.11] Insert 6000 numbers at random positions",
+        "./yrs/benches/input/b-1-11-6000.bin",
+    );
 }
 
 fn bench(c: &mut Criterion) {
@@ -421,5 +281,9 @@ fn bench(c: &mut Criterion) {
     b1_11(c);
 }
 
-criterion_group!(benches, bench);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = bench,
+}
 criterion_main!(benches);
