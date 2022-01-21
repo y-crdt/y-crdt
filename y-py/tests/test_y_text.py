e176b09f990899f88bcff5e748d2eef9fdc37b24
waidhoferj <waidhoferj@gmail.com>
2022-01-20 22:17:48 -0800

    complete xml featureset with all tests passing

--- y-py/tests/test_y_text.py
+++ y-py/tests/test_y_text.py
@@ -7,19 +7,20 @@ import y_py as Y
 def test_inserts():
     d1 = Y.YDoc()
     x = d1.get_text('test')
-
-    d1.transact(lambda txn : x.push(txn, "hello "))
-    d1.transact(lambda txn : x.push(txn, "world!"))
+    with d1.begin_transaction() as txn:
+        x.push(txn, "hello ")
+        x.push(txn, "world!")
+        value = x.to_string(txn)
     expected = "hello world!"
-    value = d1.transact(lambda txn : x.to_string(txn))
     assert value == expected
 
     d2 = Y.YDoc(2)
     x = d2.get_text('test')
 
     exchange_updates([d1, d2])
+    with d2.begin_transaction() as txn:
+        value = x.to_string(txn)
 
-    value = d2.transact(lambda txn : x.to_string(txn))
     assert value == expected
 
 def test_deletes():
